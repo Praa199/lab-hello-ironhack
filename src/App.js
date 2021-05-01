a244da30d779da3838b5d67aa9bff6c1e428adcc
Aldo <aldo.pena.93@hotmail.com>
2021-05-01 16:08:43 +0200

    not done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <nav className="App-nav">
+        <div>
+          <img src="/images/ironhack-logo.svg"></img>
+        </div>
+        <div>
+          <img src="/images/menu-top.svg"></img>
+        </div>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Say hello to ReactJS</h1>
+          <p>
+            Do ipsum veniam magna culpa duis exercitation pariatur. Non in
+            pariatur sunt minim sunt sint. Duis nisi laboris esse ipsum laborum
+            commodo duis qui magna quis enim qui.
+          </p>
+          <a>
+            <button>Awesome!!</button>
+          </a>
+          <Footer />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Footer() {
+    
+    return(
+
+        <footer className="App-footer">
+      <div className="footer-divs">
+        <img src="/images/icon1.png" className="footer-images" />
+        <h4>Duis nisi laboris</h4>
+        <p>
+          Elit officia ex duis anim irure incididunt excepteur ut elit aute do
+          elit.
+        </p>
+      </div>
+      <div className="footer-divs">
+        <img src="/images/icon2.png" className="footer-images" />
+        <h4>Duis nisi laboris</h4>
+        <p>
+          Elit officia ex duis anim irure incididunt excepteur ut elit aute do
+          elit.
+        </p>
+      </div>
+      <div className="footer-divs">
+        <img src="/images/icon3.png" className="footer-images" />
+        <h4>Duis nisi laboris</h4>
+        <p>
+          Elit officia ex duis anim irure incididunt excepteur ut elit aute do
+          elit.
+        </p>
+      </div>
+      <div className="footer-divs">
+        <img src="/images/icon4.png" className="footer-images" />
+        <h4>Duis nisi laboris</h4>
+        <p>
+          Elit officia ex duis anim irure incididunt excepteur ut elit aute do
+          elit.
+        </p>
+      </div>
+    </footer>
+)
+}
+
+export default Footer
